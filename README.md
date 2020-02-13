2daeba58fb89d8f029e7b08660e662c08d40f7f3
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-02-13 11:17:26 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# EVENT BOOKING
+Test application using node, react, express, mongodb and graphql <br/>
+Node version: >= 12
+
+# To run only the server, go to /backend folder and run 
+    
+    npm start   
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String 
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Sailing", "Coding"];
+      },
+      createEvent: args => {
+          const eventName = args.name;
+          return eventName;
+      }
+    },
+    graphiql: true // habilita console web para testar as queries
+  })
+);
+
+app.listen(3000);
